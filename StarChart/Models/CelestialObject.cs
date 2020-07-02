cc09103b7ea87572479280d8eff2ef42bf73ccee
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-07-02 14:24:44 -0500

    create celestialObject model

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satellites;
+        public TimeSpan OrbitalPeriod;
+    }
+}
